33f03da401bdeadb72ebaf076a0a333da1cacc73
Glitch (hello-webpage) <none>
2020-07-06 17:42:34 +0000

    🎣📙 Checkpoint ./script.js:18576123/1494

--- script.js
+++ script.js
@@ -3,19 +3,17 @@
 
 function setup() {
   // Code here runs only once
-  createCanvas(800, 600)
+  createCanvas(800, 600);
 }
 
 function draw() {
   // Code here runs continuously
-  background(220)
+  background("#33cccc");
 
-  // ellipse(50, 50, 50, 50)
-
+  ellipse(100, 100, 50, 50);
+  rect(20, 20, 20, 20);
+  
 }
-
-
-
 
 /* Content within slash asterisk and asterisk slash is also a comment, and
    it can span multiple lines.
@@ -24,4 +22,4 @@ function draw() {
    some stuff that is not in this file, so don't warn us that it doesn't
    exist. */
 
-/* global background, createCanvas, noFill, strokeWeight, stroke, ellipse */
+/* global background, createCanvas, noFill, strokeWeight, stroke, ellipse, rect */
